e914a1f45bb00e750d279e19795b13acd7268329
Magnus Leikvoll <mleik@equinor.com>
2021-10-06 14:39:24 +0200

    Add UidMsg

---
+++ MqttApi/MqttApi/Models/UidMsg.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MqttApi.Models
+{
+    public class UidMsg
+    {
+        [JsonProperty("x", Required = Required.Always)]
+        public float X { get; set; }
+        [JsonProperty("y", Required = Required.Always)]
+        public float Y { get; set; }
+    }
+}
