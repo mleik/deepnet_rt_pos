2755068a8b0f5454890b1972826fa08349a6e9fe
Magnus Leikvoll <mleik@equinor.com>
2021-10-07 08:40:02 +0200

    Create Services

---
+++ MqttApi/MqttApi/Services/LoggerService.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MqttApi.Services
+{
+    public class LoggerService
+    {
+        public static void InitLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+    }
+}
