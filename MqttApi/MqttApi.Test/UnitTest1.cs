ed4ca19bf7c707ea072177c444e5b6045de2215e
Magnus Leikvoll <mleik@equinor.com>
2021-10-08 09:43:26 +0200

    Add tests for UidMsg

--- MqttApi/MqttApi.Test/UnitTest1.cs
+++ MqttApi/MqttApi.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using MqttApi.Models;
+using Newtonsoft.Json;
 using System;
 using Xunit;
 
@@ -5,10 +7,27 @@ namespace MqttApi.Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("{'x': 1234.56}")]
+        [InlineData("{'y': 1234.56}")]
+        public void UidMsgShouldRaiseExceptionOnMissingField(string json)
         {
+            Assert.Throws<JsonSerializationException>(() => JsonConvert.DeserializeObject<UidMsg>(json));
+        }
+        [Fact]
+        public void UidMsgShouldRaiseExceptionOnWrongType()
+        {
+            string json = "{'x': 1234.56, 'y': 'bad form'}";
+            Assert.Throws<JsonReaderException>(() => JsonConvert.DeserializeObject<UidMsg>(json));
+        }
 
+        [Fact]
+        public void UidMsgShouldBeCreatedOnCorrectJson()
+        {
+            string json = "{'x': 34.56, 'y': 12.78}";
+            UidMsg msg = JsonConvert.DeserializeObject<UidMsg>(json);
+            Assert.InRange(34.56 - msg.X, -1e-4, 1e-4);
+            Assert.InRange(12.78 - msg.Y, -1e-4, 1e-4);
         }
     }
 }
